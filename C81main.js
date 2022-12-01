3158a88c423393edc378432c620ce3ff6f324055
Rithwik <105922131+Rithwik-Mar11@users.noreply.github.com>
2022-12-01 16:52:56 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,80 @@
+
+canvas =document.getElementById("Ncanvas");
+ctx = canvas.getContext("2d");
+colour = "black";
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(100,200,50,0,2*Math.PI);
+ctx.stroke();
+ctx = canvas.getContext("2d");
+colour = "blue";
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(100,200,50,0,2*Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(150,250,50,0,2*Math.PI);
+ctx.stroke();
+ctx = canvas.getContext("2d");
+colour = "yellow";
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(150,250,50,0,2*Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(210,200,50,0,2*Math.PI);
+ctx.stroke();
+ctx = canvas.getContext("2d");
+colour = "black";
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(210,200,50,0,2*Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(260,250,50,0,2*Math.PI);
+ctx.stroke();
+ctx = canvas.getContext("2d");
+colour = "green";
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(260,250,50,0,2*Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(320,200,50,0,2*Math.PI);
+ctx.stroke();
+ctx = canvas.getContext("2d");
+colour = "red";
+
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(320,200,50,0,2*Math.PI);
+ctx.stroke();
+
+
